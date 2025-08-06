439053ad16d6b22e55706d269d4009fa5cefe265
liver121888 <liver121888@gmail.com>
2025-08-06 15:42:32 -0700

    Initial commit

---
+++ launch/kaliber_nav2_gazebo.launch.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    # Find package share directory
+    pkg_q9_bt = get_package_share_directory('q9_bt')
+
+    # Path to bt xml file
+    # xml_file = os.path.join(pkg_q9_bt, 'bt_xml', 'q9_search.xml')
+
+    # Locate the nav2_bringup package
+    nav2_bringup_dir = get_package_share_directory('nav2_bringup')
+    
+    # Full path to the target launch file
+    tb3_sim_launch = os.path.join(nav2_bringup_dir, 'launch', 'tb3_simulation_launch.py')
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(tb3_sim_launch),
+            launch_arguments={
+                'headless': 'False'  # override headless argument
+            }.items()
+        ),
+        # Node(
+        #     package='q9_bt',
+        #     executable='dummy_server',
+        #     name='dummy_server',
+        #     output='screen',
+        # ),
+        # Node(
+        #     package='q9_bt',
+        #     executable='bt_runner_node',
+        #     name='bt_runner_node',
+        #     output='screen',
+        #     parameters=[{
+        #         'bt_xml_dir': xml_file
+        #         }],
+        # )
+    ])
